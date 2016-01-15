4124ea486031f5d5546ab1a12cc061aa4e3c3dba
Chris Schwab <cschwab24@gmail.com>
2016-01-15 00:43:45 -0500

    deleted local repo to start from scratch; practice run on my own :)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/cool', function(request, response) {
   response.send(cool());
-  console.log('sup bro-han');
+  console.log('jah live');
 });
 
 app.listen(app.get('port'), function() {
