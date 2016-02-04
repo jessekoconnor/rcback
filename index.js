d3dfbba29d2b92bd9ba1d850225c1e235b0eaf53
Jesse O'Connor <jesse.oconnor@meltwater.com>
2016-02-03 23:00:07 -0500

    Endpoint for storing a user has been started

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-app.use(cors());
-var cool = require('cool-ascii-faces');
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
 
 app.get('/cool', function(request, response) {
   console.log('/cool has been hit');
   response.send(cool());
+});
+
+app.post('/storeUser', upload.array(), function(req, res) {
+  console.log('/storeUser has been hit');
+  console.log(JSON.stringify(req.body));
+  res.json(req.body);
 });
 
 app.listen(app.get('port'), 'rollcall.local', function() {
