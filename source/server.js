96377459bab581d4addf1f312fdbfb978840fc48
Jesse O'Connor <jesse.oconnor@meltwater.com>
2016-03-27 13:34:18 -0400

    Trying to get database running

--- source/server.js
+++ source/server.js
@@ -10,6 +10,20 @@ const PORT = process.env.PORT || 5000;
 // App
 const app = express();
 
+// Database trial
+const pg = require('pg');
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
 // Add routes
 router.addRoutes(app);
 
